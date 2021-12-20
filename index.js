d2ccaea957d93aa6593163eab32c91cdbdc2afda
Alaina <alaismit1@gmail.com>
2021-12-20 09:22:45 -0700

    revise

--- index.js
+++ index.js
@@ -36,3 +36,6 @@ progressBarContainer.addEventListener("click", (e) => {
     behavior: 'smooth'
   });
 });
+
+
+
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+app.get('/', function(req, res) {    
+  res.sendFile(path.join(__dirname, './public/front.html'))
+})
+
+app.get('/styles', function (req,res) {
+  res.sendFile(path.join(__dirname, "./public/front.css"))
+})
+
+
+
+const port = process.env.PORT || 3210
+
+
+app.listen(port, () => {
+    console.log(`Commencing countdown, engines on: ${port}`)})
