f4360ec6c1aa166afdb74d22ed3e3312ca3af6ff
Alaina <alaismit1@gmail.com>
2021-12-20 10:01:04 -0700

    revise

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express()
 
 
 app.get('/', function(req, res) {    
-  res.sendFile(path.join(__dirname, './public/front.html'))
+  res.sendFile(path.join(__dirname, "./public/home.html"))
 })
 
 app.get('/styles', function (req,res) {
-  res.sendFile(path.join(__dirname, "./public/front.css"))
+  res.sendFile(path.join(__dirname, "./public/home.css"))
 })
 
 
@@ -19,3 +19,7 @@ const port = process.env.PORT || 3210
 
 app.listen(port, () => {
     console.log(`Commencing countdown, engines on: ${port}`)})
+
+
+
+    
